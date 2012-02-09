18f89a5dfebf9fee57c3fe70bf6c674490df0d54
ryu22e <ryu22e@gmail.com>
2012-02-09 17:43:14 +0900

    Add templates.

---
+++ dot.vim/.vim/template/skeleton.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        TODO Input @name.
+// @namespace   TODO Input @namespace.
+// @description TODO Input @description.
+// @include    TODO Input @include.
+// @require    http://ajax.googleapis.com/ajax/libs/jquery/1.3.1/jquery.min.js
+// @version    1.0
+// ==/UserScript==
+// TODO Confirm year.
+// Copyright (c) 2010  Ryuji Tsutsui <ryu22e@gmail.com>.
+// Distributed under new BSD license
+// http://blog.livedoor.jp/ryu22e/
+
+(function($) {
+})(jQuery);
