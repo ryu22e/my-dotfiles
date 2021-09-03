9613fbfd75b7c4015fea90b932ee09b127b2e19f
Ryuji Tsutsui <ryu22e@gmail.com>
2021-09-03 09:28:51 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 set -e
 
-ln -s `pwd`/dot.zsh/.zsh $HOME/
-ln -s `pwd`/dot.zsh/.zshrc $HOME/
+mkdir -p $HOME/.config
 
-ln -s `pwd`/dot.vim/.vim $HOME/
-ln -s `pwd`/dot.vim/.vimrc $HOME/
-ln -s `pwd`/dot.vim/.gvimrc $HOME/
+ln -s $(pwd)/dot.zsh/.zsh $HOME/
+ln -s $(pwd)/dot.zsh/.zshrc $HOME/
 
-ln -s `pwd`/dot.ack/.ackrc $HOME/
+ln -s $(pwd)/dot.vim/.vim $HOME/
+ln -s $(pwd)/dot.vim/.vimrc $HOME/
+ln -s $(pwd)/dot.vim/.gvimrc $HOME/
+ln -s $(pwd)/dot.vim/.vimrc $HOME/.config/nvim/init.vim
+
+ln -s $(pwd)/dot.ack/.ackrc $HOME/
+
+ln -s $(pwd)/dot.direnv/direnv $HOME/.config/direnv
